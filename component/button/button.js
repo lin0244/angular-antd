c12a6d5cd28e0544d0de6451bf86d2f176ac06c6
jiangseventeen <jiangseventeen@gmail.com>
2017-04-20 17:43:46 +0800

    update component button

--- component/button/button.js
+++ component/button/button.js
@@ -23,7 +23,8 @@
  * @example
  * <a-button type="primary"></a-button> : a primary button
  * <a-button loading="true"></a-button> : a loading button
- *
+ * 
+ * ant-btn ant-btn-
  */
 
 angular.module('button',[])
@@ -40,19 +41,31 @@ angular.module('button',[])
 			size:'@',
 			loading:'@',
 			ghost:'@',
-			style:'@'	//it ok.
+			style:'@',	//it's ok.
+			onclick:'&'
 		},
-		template:'<button ng-click="click()" ng-style="style" class="ant-btn ant-btn-{{type}} {{clicked}} {{btnSize[size]}} {{btnShape[shape]}}" type="{{htmlType}}"><i class="anticon anticon-spin anticon-loading" ng-if="loading==\'true\'"></i><i class="anticon anticon-{{icon}}" ng-if="(!(loading==\'true\')) && !!icon"></i><span ng-transclude ng-if="shape==\'circle\'?false:true"></span>\
+		template:'<button ng-click="click()" ng-style="style" ng-class="btnCls" type="{{htmlType}}">\
+					<i class="anticon anticon-spin anticon-loading" ng-if="loading==\'true\'"></i>\
+					<i class="anticon anticon-{{icon}}" ng-if="(!(loading==\'true\')) && !!icon"></i>\
+					<span ng-transclude ng-if="shape==\'circle\'?false:true"></span>\
 				</button>',
 		link: function(scope,element,attrs){
-			console.log(attrs.$normalize());
-			scope.btnSize = {large:'ant-btn-lg',small:'ant-btn-sm'};
-			scope.btnShape = {circle:'ant-btn-circle ant-btn-icon-only'};
+			var prefixCls = 'ant-btn';
+			var btnSize = {large:'lg',small:'sm'};
+			scope.clicked = false;
+			scope.btnCls = {};
+			scope.btnCls[prefixCls] = true;
+			scope.btnCls[prefixCls + '-' + scope.type] = scope.type;
+			scope.btnCls[prefixCls + '-' + btnSize[scope.size]] = scope.size;
+			scope.btnCls[prefixCls + '-' + scope.shape] = scope.shape;
+			scope.btnCls[prefixCls + '-clicked'] = scope.clicked;
+			scope.btnCls[prefixCls + '-background-ghost'] = scope.ghost;
 			scope.click = function(){
-				scope.clicked = "ant-btn-clicked";
+				scope.clicked = true;
 				scope.clearTimeout = $timeout(function(){
-					scope.clicked = "";
+					scope.clicked = false;
 				},500);
+				console.log(scope.btnCls);
 			}
 		}
 	}
